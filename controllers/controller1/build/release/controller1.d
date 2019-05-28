d671cc2b0f2be3766514454169a04d66c3228834
Angel Gustavo Ek Tun <al1609044@upy.edu.mx>
2019-05-28 15:10:41 -0500

    second counter functionality added

---
+++ controllers/controller1/build/release/controller1.d
@@ -0,0 +1,4 @@
+build/release/controller1.o: controller1.c \
+ /usr/local/webots/include/controller/c/webots/robot.h \
+ /usr/local/webots/include/controller/c/webots/types.h \
+ /usr/local/webots/include/controller/c/webots/nodes.h
