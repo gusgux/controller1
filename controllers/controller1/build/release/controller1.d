8382a5dbc7fa2d1d4046ffa83f2d94130a2cb50c
Angel Gustavo Ek Tun <al1609044@upy.edu.mx>
2019-05-28 16:09:48 -0500

    led on/off

--- controllers/controller1/build/release/controller1.d
+++ controllers/controller1/build/release/controller1.d
@@ -1,4 +1,5 @@
 build/release/controller1.o: controller1.c \
  /usr/local/webots/include/controller/c/webots/robot.h \
  /usr/local/webots/include/controller/c/webots/types.h \
- /usr/local/webots/include/controller/c/webots/nodes.h
+ /usr/local/webots/include/controller/c/webots/nodes.h \
+ /usr/local/webots/include/controller/c/webots/led.h
--- controllers/controller1/controller1.c
+++ controllers/controller1/controller1.c
@@ -10,7 +10,9 @@
  * You may need to add include files like <webots/distance_sensor.h> or
  * <webots/differential_wheels.h>, etc.
  */
+#include  <stdio.h>
 #include <webots/robot.h>
+#include <webots/led.h>
 
 /*
  * You may want to add macros here.
@@ -26,22 +28,26 @@ int main(int argc, char **argv)
 {
   /* necessary to initialize webots stuff */
   wb_robot_init();
-
+ 
   /*
    * You should declare here WbDeviceTag variables for storing
    * robot devices like this:
    *  WbDeviceTag my_sensor = wb_robot_get_device("my_sensor");
    *  WbDeviceTag my_actuator = wb_robot_get_device("my_actuator");
    */
-   printf("doing some setup!\n");
+   printf("doing some setup! \n");
    int seconds_counter=0;
+   int led_state=0;
+   
+   WbDeviceTag my_led=wb_robot_get_device("led");
 
   /* main loop
    * Perform simulation steps of TIME_STEP milliseconds
    * and leave the loop when the simulation is over
    */
   while (wb_robot_step(TIME_STEP) != -1) {
-   //printf("doing some repetitve task!\n");
+  
+   printf("doing some repetitve task! \n");
    printf("Time= %i \n",seconds_counter);
    seconds_counter++;
 
@@ -51,12 +57,26 @@ int main(int argc, char **argv)
      *  double val = wb_distance_sensor_get_value(my_sensor);
      */
 
+ //wb_led_set(my_led,0);
+
+
+led_state=wb_led_get(my_led);
+printf("led value= %i \n",led_state);
+  if(led_state==0)
+    
+    wb_led_set(my_led,1);
+    
+      else{
+        wb_led_set(my_led,0);
+      
+      }
     /* Process sensor data here */
 
     /*
      * Enter here functions to send actuator commands, like:
      * wb_differential_wheels_set_speed(100.0,100.0);
      */
+     
   };
 
   /* Enter your cleanup code here */
